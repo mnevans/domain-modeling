63a93d9627a4e09a45270163fa9f8487193c33e4
Morgan Evans <evansmorg@gmail.com>
2015-10-14 23:29:41 -0700

    money.swift almost finished

---
+++ Person.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  domain-modeling.swift
+//
+//  Created by Morgan Evans on 10/14/15.
+//  Copyright © 2015 Morgan Evans. All rights reserved.
+//
+
+import Foundation
---
+++ domain-modeling/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  domain-modeling
+//
+//  Created by Morgan Evans on 10/14/15.
+//  Copyright © 2015 Morgan Evans. All rights reserved.
+//
+
+import Foundation
+
